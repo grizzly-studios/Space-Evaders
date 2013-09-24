b5140d2a33b7b33596245cc61331d93f12206e5b
William Poynter <poynter.william@gmail.com>
2013-09-25 00:31:15 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: williampoynter
+ *
+ * Created on September 25, 2013, 12:29 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+        return 0;
+}
+
