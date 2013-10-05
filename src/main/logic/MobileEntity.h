2d57ec207ba5458686b021ebccd12644af7a8a85
William Poynter <poynter.william@gmail.com>
2013-10-05 11:10:37 +0100

    Documented MobileEntity class

--- src/main/logic/MobileEntity.h
+++ src/main/logic/MobileEntity.h
@@ -11,13 +11,30 @@
 #include "Entity.h"
 
 namespace gs {
-
+	
+	/**
+	 * Base class for all moving objects
+     */
 	class MobileEntity : public Entity {
 	public:
+		/**
+		 * Default constructor
+         */
 		MobileEntity();
+		/**
+		 * Copy constructor
+         * @param orig Original mobile object to deep copy
+         */
 		MobileEntity(const MobileEntity& orig);
 		virtual ~MobileEntity();
 		
+		/**
+		 * Movement function
+		 * This is the key function of a mobile object. The time elapsed since
+		 * the object was last moved is used to calculate the new position of
+		 * the object.
+         * @param dt Time interval since last move call
+         */
 		virtual void move(const double & dt)=0;
 	private:
 
