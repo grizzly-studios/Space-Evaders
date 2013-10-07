f8d6af002eefe62b67371c35192a7cbf140cfc5b
Al <smokyturbo@gmail.com>
2013-10-07 21:01:53 +0100

    Hacked main to demonstrate events.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -6,9 +6,13 @@
  */
 
 #include <cstdlib>
+
+#include <iostream>
 #include <tr1/memory>
 
 #include "event/EventManager.hpp"
+#include "event/BiscuitEvent.hpp"
+#include "event/CheeseEvent.hpp"
 #include "logic/Logic.hpp"
 #include "view/View.hpp"
 #include "app/Application.h"
@@ -21,15 +25,27 @@ using namespace gs;
  */
 int main(int argc, char** argv) {
 	IEventManagerPtr eventManager(new EventManager);
+
 	ILogicPtr logic(new Logic(eventManager));
 	IViewPtr view(new View(eventManager));
 
-	Application *app = new Application(argc, argv, logic, view);
+	// TODO: Find out which cast type we need
+	eventManager->addListener(BISCUIT_EVENT, std::tr1::dynamic_pointer_cast<IEventListener>(logic));
+	eventManager->addListener(CHEESE_EVENT, std::tr1::dynamic_pointer_cast<IEventListener>(view));
+
+	BiscuitEvent biscuitEvent;
+	eventManager->fireEvent(biscuitEvent);
+
+	CheeseEvent cheeseEvent;
+	eventManager->fireEvent(cheeseEvent);
+
+	//Application *app = new Application(argc, argv, logic, view);
 
         // app->init();
         
         // app->run();
 
+	std::cout << "returning..." << std::endl;
+
     return 0;
 }
-
