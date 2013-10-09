e68039a97c726751e21dc6d863dbcba3ca7ed1e2
Al <smokyturbo@gmail.com>
2013-10-09 18:59:23 +0100

    Removed test code form main.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -5,14 +5,7 @@
  * Created on September 25, 2013, 12:29 AM
  */
 
-#include <cstdlib>
-
-#include <iostream>
-#include <tr1/memory>
-
 #include "event/EventManager.hpp"
-#include "event/BiscuitEvent.hpp"
-#include "event/CheeseEvent.hpp"
 #include "logic/Logic.hpp"
 #include "view/View.hpp"
 #include "app/Application.h"
@@ -29,23 +22,9 @@ int main(int argc, char** argv) {
 	ILogicPtr logic(new Logic(eventManager));
 	IViewPtr view(new View(eventManager));
 
-	// TODO: Find out which cast type we need
-	eventManager->addListener(BISCUIT_EVENT, std::tr1::dynamic_pointer_cast<IEventListener>(logic));
-	eventManager->addListener(CHEESE_EVENT, std::tr1::dynamic_pointer_cast<IEventListener>(view));
-
-	BiscuitEvent biscuitEvent;
-	eventManager->fireEvent(biscuitEvent);
-
-	CheeseEvent cheeseEvent;
-	eventManager->fireEvent(cheeseEvent);
-
-	//Application *app = new Application(argc, argv, logic, view);
-
-        // app->init();
-        
-        // app->run();
-
-	std::cout << "returning..." << std::endl;
+	Application *app = new Application(argc, argv, logic, view);
+	app->init();
+	app->run();
 
     return 0;
 }
