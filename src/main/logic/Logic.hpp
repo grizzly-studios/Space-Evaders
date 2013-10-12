c2b4420a6ba3a4d08818ef5a66f9f03c814b5890
William Poynter <poynter.william@gmail.com>
2013-10-12 23:57:21 +0100

    Merged Wheelman's and Sol's separate logic controllers.
    Fixed a mistake in the timing calculations too.

--- src/main/logic/Logic.hpp
+++ src/main/logic/Logic.hpp
@@ -2,12 +2,27 @@
 #define	LOGIC_H
 
 #include <iostream>
+#include <list>
+#include <algorithm>
+#include <iomanip>
+
+#include <SFML/System/Clock.hpp>
+#include <SFML/System/Time.hpp>
 
 #include "ILogic.hpp"
 #include "../event/IEventListener.hpp"
 #include "../event/IEventManager.hpp"
 
+#include "MobileEntity.h"
+#include "Player.h"
+#include "Bullets.h"
+
 namespace gs {
+	
+typedef std::list<EntityShPtr> EntityList;
+typedef std::list<MobileEntityShPtr> MobileEntityList;
+typedef std::list<PlayerShPtr> PlayerList;
+typedef std::list<BulletsShPtr> BulletsList;
 
 class Logic : public ILogic, public IEventListener  {
 public:
@@ -17,6 +32,22 @@ public:
 	virtual void onEvent(Event& event);
 private:
 	IEventManagerPtr eventManager;
+	
+	sf::Clock *clock;
+	double accumulator;
+	double dt;
+
+	EntityList allObjects;
+	MobileEntityList mobileObjects;
+	PlayerList allPlayers;
+	BulletsList allBullets;
+	
+	//Subroutines
+	void move();
+	void collisionDetection();
+
+	void integrate();
+	void interpolate(const double &remainder);
 };
 
 }
