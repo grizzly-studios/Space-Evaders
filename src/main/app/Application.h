70a084f34250acb143efb0accca2a0d94a5566c1
William Poynter <poynter.william@gmail.com>
2013-10-31 12:32:15 +0000

    Documented Application

--- src/main/app/Application.h
+++ src/main/app/Application.h
@@ -27,24 +27,62 @@ namespace gs {
 
 typedef std::tr1::shared_ptr<sf::RenderWindow> RenderWindowShPtr;
 
+/**
+ * Application layer implementation
+ * 
+ * The Application object is the highest level object to be created. It handles the entire program
+ * and houses the Event Manager, Logic and View. It also takes care of a number of other running
+ * preferences.
+ * 
+ */
 class Application {
 public:
+	/**
+	 * Constructor
+	 * 
+	 * This is the only constructor for the Application. It requires the passing of the program
+	 * arguments from the terminal.
+     * @param argc Number of arguments being passed
+     * @param argv An array of arguments
+     */
 	Application(int argc, char** argv);
+	/**
+	 * Destructor
+     */
 	virtual ~Application();
-
+	/**
+	 * Initialise Application
+	 * 
+	 * Prepares the Application for running by creating the View, Logic and Event Manger and
+	 * performing startup tasks.
+     */
 	void init();
+	/**
+	 * Run Application
+	 * 
+	 * Handles the program kernel.
+     */
 	void run();
 
 private:
-        IEventManagerPtr eventManager;
-        ILogicPtr logic;
-        IViewPtr view;
-        bool flush;
-        unsigned int FR;
-        unsigned int AL;
-        unsigned int WIDTH, HEIGHT;
-        sf::ContextSettings settings;
-        RenderWindowShPtr window;
+	/**Shared pointer to Event Manager*/
+	IEventManagerPtr eventManager;
+	/**Shared pointer to Logic*/
+	ILogicPtr logic;
+	/**Shared pointer to View*/
+	IViewPtr view;
+	/**Boolean to indicate forced log flushing*/
+	bool flush;
+	/**Screen frame rate*/
+	unsigned int FR;
+	/**Antialiasing level*/
+	unsigned int AL;
+	/**Screen width and height*/
+	unsigned int WIDTH, HEIGHT;
+	/**SFML settings for render window*/
+	sf::ContextSettings settings;
+	/**Shared pointer to render window*/
+	RenderWindowShPtr window;
 };
 
 }
