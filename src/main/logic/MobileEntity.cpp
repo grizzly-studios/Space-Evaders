3cb202ef815a4d8510176046a16e36243d9d83d1
William Poynter <poynter.william@gmail.com>
2015-04-03 17:01:42 +0100

    Written safeSetForce function

--- src/main/logic/MobileEntity.cpp
+++ src/main/logic/MobileEntity.cpp
@@ -85,9 +85,21 @@ void MobileEntity::setForce(const sf::Vector2f &_force) {
 }
 
 void MobileEntity::safeSetForce(const sf::Vector2f &_force) {
-	//sf::Vector2f newFroce = _force;
+	sf::Vector2f newFroce = _force;
 	//Add code to protect against forcing through objects
-	force = _force;
+	if (isDirDisabled(LEFT) && newFroce.x < 0) {
+		newFroce.x = 0;
+	}
+	if (isDirDisabled(UP) && newFroce.y < 0) {
+		newFroce.y = 0;
+	}
+	if (isDirDisabled(RIGHT) && newFroce.x > 0) {
+		newFroce.x = 0;
+	}
+	if (isDirDisabled(DOWN) && newFroce.y > 0) {
+		newFroce.y = 0;
+	}
+	force = newFroce;
 }
 
 sf::Vector2f MobileEntity::getVector(const Direction &dir, const float &mag) const {
