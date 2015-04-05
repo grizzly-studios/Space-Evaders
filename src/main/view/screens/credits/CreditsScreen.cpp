b584e4e3c63f2da21260e18ed09cf18feaf7e5d1
William Poynter <poynter.william@gmail.com>
2015-04-05 15:51:45 +0100

    Added text boarders to make more readable

--- src/main/view/screens/credits/CreditsScreen.cpp
+++ src/main/view/screens/credits/CreditsScreen.cpp
@@ -40,23 +40,42 @@ void CreditsScreen::render(RenderWindowShPtr window) {
 	text.setColor(sf::Color::Red);
 	text.setStyle(sf::Text::Bold);
 
+	sf::RectangleShape textBorder;
+	textBorder.setFillColor(sf::Color::Black);
+
+	textBorder.setSize(sf::Vector2f(170, 22));
+	textBorder.setOrigin(sf::Vector2f(textBorder.getGlobalBounds().width/2, 0));
+	textBorder.setPosition(styleManager->getWidth()/2,222);
 	text.setString("AL LAMBERT");
 	text.setOrigin(sf::Vector2f(text.getGlobalBounds().width/2,0));
 	text.setPosition(styleManager->getWidth()/2,218);
+	window->draw(textBorder);
 	window->draw(text);
 
+	textBorder.setSize(sf::Vector2f(280, 22));
+	textBorder.setOrigin(sf::Vector2f(textBorder.getGlobalBounds().width/2, 0));
+	textBorder.setPosition(styleManager->getWidth()/2,272);
 	text.setString("CHRIS REEVE-MATES");
 	text.setOrigin(sf::Vector2f(text.getGlobalBounds().width/2,0));
 	text.setPosition(styleManager->getWidth()/2,268);
+	window->draw(textBorder);
 	window->draw(text);
 
+	textBorder.setSize(sf::Vector2f(180, 22));
+	textBorder.setOrigin(sf::Vector2f(textBorder.getGlobalBounds().width/2, 0));
+	textBorder.setPosition(styleManager->getWidth()/2,322);
 	text.setString("ROB PARKER");
 	text.setOrigin(sf::Vector2f(text.getGlobalBounds().width/2,0));
 	text.setPosition(styleManager->getWidth()/2,318);
+	window->draw(textBorder);
 	window->draw(text);
 
+	textBorder.setSize(sf::Vector2f(200, 22));
+	textBorder.setOrigin(sf::Vector2f(textBorder.getGlobalBounds().width/2, 0));
+	textBorder.setPosition(styleManager->getWidth()/2,372);
 	text.setString("WILL POYNTER");
 	text.setOrigin(sf::Vector2f(text.getGlobalBounds().width/2,0));
 	text.setPosition(styleManager->getWidth()/2,368);
+	window->draw(textBorder);
 	window->draw(text);
 }
