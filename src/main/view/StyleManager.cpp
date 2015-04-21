053a20f351817752d2f39f41785bdadf08c20653
smokyturbo <alan.lambert256@gmail.com>
2015-04-21 21:08:22 +0100

    Retrieve absolute file paths for assets (Windows only).

--- src/main/view/StyleManager.cpp
+++ src/main/view/StyleManager.cpp
@@ -7,9 +7,42 @@
 
 #include "StyleManager.h"
 
+#include <algorithm>
+
+#if defined(_WIN64) || defined(_WIN32)
+//Windows specific code
+#include <windows.h>
+//End Windows specific code	  
+#endif
+
 using namespace gs;
 
-const std::string StyleManager::assetPath = "assets/";
+const std::string StyleManager::assetPath = "/assets/";
+
+namespace {
+
+std::string getExecutablePath() {
+	static std::string execPath;
+
+	if (execPath.size() > 0) {
+		return execPath;
+	}
+
+	#if defined(_WIN64) || defined(_WIN32)
+		HMODULE hModule = GetModuleHandleW(NULL);
+		WCHAR path[MAX_PATH];
+		int lengthWcharPath = GetModuleFileNameW(hModule, path, MAX_PATH);
+		int lengthStringPath = WideCharToMultiByte(CP_UTF8, 0, path, lengthWcharPath, NULL, 0, NULL, NULL);
+		execPath = std::string(lengthStringPath, 0);
+		WideCharToMultiByte(CP_UTF8, 0, path, lengthWcharPath, &execPath[0], lengthStringPath, NULL, NULL);
+		std::replace(execPath.begin(), execPath.end(), '\\', '/');
+		execPath = execPath.substr(0, execPath.find_last_of("/"));
+	#endif
+
+	return execPath;
+}
+
+}
 
 StyleManager::StyleManager() {
 	setFont(StyleGroup::DEFAULT, "arial.ttf");
@@ -28,15 +61,15 @@ short StyleManager::getHeight() {
 }
 
 std::string StyleManager::getAssetPath() {
-	return assetPath;
+	return getExecutablePath() + assetPath;
 }
 
 std::string StyleManager::getAssetPath(std::string file) {
-	return assetPath + file;
+	return getAssetPath() + file;
 }
 
 void StyleManager::setFont(StyleGroup styleGroup, std::string fontFilePath) {
-	if (!fonts[styleGroup].loadFromFile(assetPath + fontFilePath)) {
+	if (!fonts[styleGroup].loadFromFile(getAssetPath(fontFilePath))) {
 		ERR << "Cannot load font file: " << fontFilePath << std::endl;
 	}
 }
